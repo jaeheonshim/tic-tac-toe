dfd3446bae17832240776cbb0f52162cb1e02807
Jaeheon Shim <git@jaeheonshim.com>
2022-11-13 23:07:29 -0500

    Move board utilities to separate file

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useRef, useState } from 'react';
 import Board from './Board';
 import shake from './util/shake';
 import "./App.css"
+import { getWinner, toPlay } from './board/boardutil';
 
 const StatusTexts = {
   X_PLAY: "X to play",
@@ -20,29 +21,6 @@ function App() {
     [0, 0, 0],
     [0, 0, 0]
   ]);
-
-  const toPlay = (board) => {
-    const reduce = (l) => l.reduce((p, c) => p + c, 0);
-    const sum = reduce(board[0]) + reduce(board[1]) + reduce(board[2]);
-    return 1 + 2 * -sum;
-  }
-
-  const getWinner = (board) => {
-    for(let i = 0; i < 3; ++i) {
-      if(board[i][0] == board[i][1] && board[i][1] == board[i][2] && board[i][2] != 0) return board[i][0]; // check column
-      if(board[0][i] == board[1][i] && board[1][i] == board[2][i] && board[2][i] != 0) return board[0][i];
-    }
-
-    if(board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[2][2] != 0) return board[0][0];
-    if(board[0][2] == board[1][1] && board[1][1] == board[2][0] && board[2][0] != 0) return board[0][2];
-
-    const reduce = (l) => l.reduce((p, c) => p + c, 0);
-    const sum = reduce(board[0]) + reduce(board[1]) + reduce(board[2]);
-
-    if(sum == 9) return true;
-
-    return false;
-  }
 
   const place = (r, c, player) => {
     const newBoard = [[...board[0]], [...board[1]], [...board[2]]];
@@ -67,7 +45,7 @@ function App() {
 
   const onBoardClick = (r, c) => {
     if(getWinner(board)) return;
-    
+
     if(board[r][c] == 0) {
       place(r, c, toPlay(board));
     } else {
