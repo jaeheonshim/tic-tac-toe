a5cbb6884fc536603dc0fccb8d3346f45a202fd3
Jaeheon Shim <git@jaeheonshim.com>
2022-11-13 22:50:52 -0500

    Add placing tokens, shake on invalid

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
-import logo from './logo.svg';
+import { useRef, useState } from 'react';
 import Board from './Board';
+import shake from './util/shake';
 
 function App() {
+  const boardRef = useRef(null);
+
+  const [board, setBoard] = useState([
+    [1, 0, 0],
+    [0, 0, 0],
+    [0, 0, 0]
+  ]);
+
+  const toPlay = (board) => {
+    const reduce = (l) => l.reduce((p, c) => p + c, 0);
+    const sum = reduce(board[0]) + reduce(board[1]) + reduce(board[2]);
+    return 1 + 2 * -sum;
+  }
+
+  const place = (r, c, player) => {
+    const newBoard = [[...board[0]], [...board[1]], [...board[2]]];
+    newBoard[r][c] = player;
+    setBoard(newBoard);
+  }
+
+  const onBoardClick = (r, c) => {
+    if(board[r][c] == 0) {
+      place(r, c, toPlay(board));
+    } else {
+      shake(boardRef.current);
+    }
+  };
+
   return (
     <div className="App">
-      <Board />
+      <Board boardRef={boardRef} board={board} onClick={onBoardClick} />
     </div>
   );
 }
--- src/Board.js
+++ src/Board.js
@@ -1,23 +1,41 @@
 import "./Board.css"
 
-export default function Board() {
+export default function Board({ board, onClick, boardRef }) {
     return (
-        <div className="board">
+        <div ref={boardRef} className="board">
             <div className="row">
-                <div className="cell br bb"></div>
-                <div className="cell bl br bb"></div>
-                <div className="cell bl bb"></div>
+                <div className="cell br bb" onClick={() => onClick(0, 0)}>
+                    {board[0][0] == 1 && <div className="x"></div>}{board[0][0] == -1 && <div className="o"></div>}
+                </div>
+                <div className="cell bl br bb" onClick={() => onClick(0, 1)}>
+                    {board[0][1] == 1 && <div className="x"></div>}{board[0][1] == -1 && <div className="o"></div>}
+                </div>
+                <div className="cell bl bb" onClick={() => onClick(0, 2)}>
+                    {board[0][2] == 1 && <div className="x"></div>}{board[0][2] == -1 && <div className="o"></div>}
+                </div>
             </div>
             <div className="row">
-                <div className="cell bt br bb"></div>
-                <div className="cell bt br bb bl"></div>
-                <div className="cell bt bb bl"></div>
+                <div className="cell bt br bb" onClick={() => onClick(1, 0)}>
+                    {board[1][0] == 1 && <div className="x"></div>}{board[1][0] == -1 && <div className="o"></div>}
+                </div>
+                <div className="cell bt br bb bl" onClick={() => onClick(1, 1)}>
+                    {board[1][1] == 1 && <div className="x"></div>}{board[1][1] == -1 && <div className="o"></div>}
+                </div>
+                <div className="cell bt bb bl" onClick={() => onClick(1, 2)}>
+                    {board[1][2] == 1 && <div className="x"></div>}{board[1][2] == -1 && <div className="o"></div>}
+                </div>
             </div>
             <div className="row">
-                <div className="cell bt br"></div>
-                <div className="cell bt br bl"></div>
-                <div className="cell bt bl"></div>
+                <div className="cell bt br" onClick={() => onClick(2, 0)}>
+                    {board[2][0] == 1 && <div className="x"></div>}{board[2][0] == -1 && <div className="o"></div>}
+                </div>
+                <div className="cell bt br bl" onClick={() => onClick(2, 1)}>
+                    {board[2][1] == 1 && <div className="x"></div>}{board[2][1] == -1 && <div className="o"></div>}
+                </div>
+                <div className="cell bt bl" onClick={() => onClick(2, 2)}>
+                    {board[2][2] == 1 && <div className="x"></div>}{board[2][2] == -1 && <div className="o"></div>}
+                </div>
             </div>
         </div>
-        )
-    }
+    )
+}
